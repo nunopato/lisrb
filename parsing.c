6f629e007a0f65fca19a8141f075fa81c6e1871c
Nuno Pato <nunopato@gmail.com>
2015-11-04 23:24:38 +0100

    Rename from prompt to parsing

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -24,6 +25,20 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lisrb    = mpc_new("lisrb");
+  
+  mpca_lang(MPCA_LANG_DEFAULT, 
+      "                                                    \
+        number   : /-?[0-9]+/ ;                            \
+        operator : '+' | '-' | '*' | '/' ;                 \
+        expr     : <number> | '(' <operator> <expr>+ ')' ; \
+        lisrb    : /^/ <operator> <expr>+ /$/ ;            \
+      ",
+      Number, Operator, Expr, Lisrb);
+
   puts("Lisrb version 0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
@@ -36,6 +51,9 @@ int main(int argc, char** argv) {
 
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lisrb);
+
   return 0;
 }
 
