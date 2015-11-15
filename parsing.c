89741972e592e224a4cad17f66125b8f1eaa414d
Nuno Pato <nunopato@gmail.com>
2015-11-15 14:51:43 +0100

    Consider an error as a possible return value from a expression evaluation

--- parsing.c
+++ parsing.c
@@ -20,6 +20,47 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+enum { LVAL_NUM, LVAL_ERR};
+
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR:
+      if (v.err == LERR_DIV_ZERO) {
+        printf("Error: division by zero!");
+      }
+      if (v.err == LERR_BAD_OP) {
+        printf("Error: invalid operator!");
+      }
+      if (v.err == LERR_BAD_NUM) {
+        printf("Error: invalid number!");
+      }
+    break;
+  }
+}
+
 long expo(long base, long exponent) {
   if (exponent == 1) { return base; }
   return base * expo(base, exponent - 1);
