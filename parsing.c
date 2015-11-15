d1556fe6fda1193849d8b7cd72b5ab1eaaf56cf4
Nuno Pato <nunopato@gmail.com>
2015-11-15 14:20:34 +0100

    Add min, max and exp

--- parsing.c
+++ parsing.c
@@ -20,20 +20,39 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long expo(long base, long exponent) {
+  if (exponent == 1) { return base; }
+  return base * expo(base, exponent - 1);
+}
+
 long eval_op(long x, char* op, long y) {
   if (strcmp(op, "+") == 0) { return x + y; }
   if (strcmp(op, "-") == 0) { return x - y; }
   if (strcmp(op, "/") == 0) { return x / y; }
   if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return expo(x, y); }
+
+  if (strcmp(op, "min") == 0) {
+    if (x <= y) { return x; }
+    return y;
+  }
+  if (strcmp(op, "max") == 0) {
+    if (x >= y) { return x; }
+    return y;
+  }
+
   return 0;
 }
 
 long eval(mpc_ast_t* t) {
 
+  // parsing a leaf here
   if (strstr(t->tag, "number")) {
     return atoi(t->contents);
   }
 
+  // the operator is always the second child
   char* op = t->children[1]->contents;
 
   long x = eval(t->children[2]);
@@ -53,11 +72,11 @@ int main(int argc, char** argv) {
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lisrb    = mpc_new("lisrb");
-  
-  mpca_lang(MPCA_LANG_DEFAULT, 
+
+  mpca_lang(MPCA_LANG_DEFAULT,
       "                                                                                  \
         number   : /-?[0-9]+/ ;                                                          \
-        operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" ; \
+        operator : '+' | '-' | '*' | '/' | '%' | '^' | \"min\" | \"max\" | \"add\" | \"sub\" | \"mul\" | \"div\" ; \
         expr     : <number> | '(' <operator> <expr>+ ')' ;                               \
         lisrb    : /^/ <operator> <expr>+ /$/ ;                                          \
       ",
@@ -73,9 +92,11 @@ int main(int argc, char** argv) {
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lisrb, &r)) {
+
       long result = eval(r.output);
       printf("%li\n", result);
       mpc_ast_delete(r.output);
+
     } else {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
